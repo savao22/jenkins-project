99d1b852550bb9d32bef1a1e19cd0febe974eb8a
savao22 <102739411+savao22@users.noreply.github.com>
2026-05-17 21:43:39 +0300

    Update AMI ID in awsec2.tf

--- awsec2.tf
+++ awsec2.tf
@@ -1,10 +1,13 @@
+provider "aws" {
+  region = "us-east-1"
+}
+
 resource "aws_instance" "terraform_demo" {
-  ami                    = "ami-0533af068c146e115"
+  ami                    = "ami-0c7217cdde317cfec"
   instance_type          = "t3.micro"
   availability_zone      = "us-east-1c"
   vpc_security_group_ids = ["sg-00ef03cc35ddaa39c"]
-  
-  key_name               = "class2p" 
+  key_name               = "class2p"
 
   tags = {
     Name = "TerraformOS"
