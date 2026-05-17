23354e4f9d5f404388699ea61e48ee3604299344
savao22 <102739411+savao22@users.noreply.github.com>
2026-05-17 21:33:40 +0300

    Update awsec2.tf

--- awsec2.tf
+++ awsec2.tf
@@ -1,15 +1,10 @@
-provider "aws" {
-  region = "us-east-1"
-}
-
 resource "aws_instance" "terraform_demo" {
-  ami                    = "ami-0eba005a3e64054e1"
+  ami                    = "ami-0533af068c146e115"
   instance_type          = "t3.micro"
   availability_zone      = "us-east-1c"
   vpc_security_group_ids = ["sg-00ef03cc35ddaa39c"]
   
-  # שינוי השם לפי מה שקיים אצלך ב-AWS Console
-  key_name               = "newkey" 
+  key_name               = "class2p" 
 
   tags = {
     Name = "TerraformOS"
